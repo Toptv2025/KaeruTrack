4c7ad2b23813b5fccee838fa88c2c30ecef731cb
kkyago <yagorangrl+1@gmail.com>
2026-03-02 21:31:51 -0300

    version cahnged

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.kaeru.app"
         minSdk = 29
         targetSdk = 36
-        versionCode = 10003
-        versionName = "1.0.3"
+        versionCode = 20000
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
